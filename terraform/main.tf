53b2e78d9a7be1e83da525bd42e7c8388d3b2a61
Jusharra <1stchoicecyber@gmail.com>
2025-11-06 20:33:32 -0800

    chore: initialize production-ready repo structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,10 +66,4 @@ resource "aws_iam_role_policy_attachment" "attach" {
   role       = aws_iam_role.workload.name
   policy_arn = aws_iam_policy.workload_policy.arn
 }
-resource "aws_security_group" "bad_sg" {
-  name   = "${var.name_prefix}-bad-sg"
-  vpc_id = aws_vpc.main.id
-  ingress { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
-  egress  { from_port = 0,  to_port = 0,  protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
-  tags = var.tags
-}
+
